2c4d5e464bfa76ddf057aeabe8b27035b827f5f9
BramS21113653 <101334896+BramS21113653@users.noreply.github.com>
2022-05-30 14:40:35 +0200

    Update Rit.java

--- src/main/java/com/example/project2/Rit.java
+++ src/main/java/com/example/project2/Rit.java
@@ -1,5 +1,16 @@
+import com.example.project2.Vervoersmiddel;
+
 import java.util.*;
 
 public class Rit {
-
+    private Integer id;
+    private Integer gerbuikersid;
+    private String vervoersmiddel;
+    private Integer afstand;
+    public  Rit(Integer id, Integer gebruikersid, String vervoersmiddel, Integer afstand){
+        this.id = id;
+        this.gerbuikersid = gebruikersid;
+        this.vervoersmiddel = vervoersmiddel;
+        this.afstand = afstand;
+    }
 }
