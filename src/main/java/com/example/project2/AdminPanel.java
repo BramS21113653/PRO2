81296dc01b6d3d494741899cf81ddd4548cef509
Leinadon <21137943@student.hhs.nl>
2022-06-07 12:32:03 +0200

    admin fix

--- src/main/java/com/example/project2/AdminPanel.java
+++ src/main/java/com/example/project2/AdminPanel.java
@@ -1,16 +1,4 @@
 package com.example.project2;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class AdminPanel extends Application {
-
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
-
-    }
+public class AdminPanel {
 }
