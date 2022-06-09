e3612b3bc4561ec772dbb1265c3e375aeb2f87bb
BramS21113653 <101334896+BramS21113653@users.noreply.github.com>
2022-06-09 11:07:03 +0200

    class + objects vervoersmiddel

---
+++ src/main/java/com/example/project2/Vervoersmiddel.java
@@ -0,0 +1,11 @@
+package com.example.project2;
+
+public class Vervoersmiddel {
+    private String vervoersmiddel;
+    private Integer uitstootPerLiter;
+
+    public Vervoersmiddel (String vervoersmiddel, Integer uitstootPerLiter) {
+        this.vervoersmiddel = vervoersmiddel;
+        this.uitstootPerLiter = uitstootPerLiter;
+    }
+}
