fbecf43346d08ed356f90e7115b6298066d124d4
NielsSchool <101334605+NielsSchool@users.noreply.github.com>
2022-06-09 14:48:01 +0200

    update

--- src/main/java/com/example/project2/Gebruiker.java
+++ src/main/java/com/example/project2/Gebruiker.java
@@ -24,7 +24,6 @@ public class Gebruiker {
         } else {
             gebruikerslijst.add(this);
         }
-        System.out.println(gebruikerslijst);
     }
 
     public void insertGebruiker() throws SQLException {
