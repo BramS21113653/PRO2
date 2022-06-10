b9817f5e0697517a50ffc1ac5da7b066c0463534
BramS21113653 <101334896+BramS21113653@users.noreply.github.com>
2022-06-10 12:58:36 +0200

    adminpanel fixes

--- src/main/java/com/example/project2/AdminPanel.java
+++ src/main/java/com/example/project2/AdminPanel.java
@@ -3,19 +3,33 @@ package com.example.project2;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
-
 public class AdminPanel {
+
+    @FXML
+    private TextField gebruikersnaam_tekst;
+
+    @FXML
+    private TextField wachtwoord_tekst;
 
     @FXML
     private Button Dashboard;
 
     @FXML
+    private TextField admin_tekst;
+
+    @FXML
     private Button Gebruiker_toevoegen;
 
     @FXML
-    private Button Gebruiker_verwijderen;
+    private Button gebruiker_verwijderen;
+
+    @FXML
+    private ComboBox<?> verwijderen_combobox;
+
     @FXML
     void Dashboard_button(ActionEvent event) throws IOException {
         HelloApplication h = new HelloApplication();
@@ -23,12 +37,32 @@ public class AdminPanel {
     }
 
     @FXML
-    void Gebruiker_verwijderen(ActionEvent event) throws IOException {
+    void gebruiker_verwijderen(ActionEvent event) {
 
     }
 
     @FXML
-    void Gebruiker_toevoegen(ActionEvent event) throws IOException {
+    void Gebruiker_toevoegen(ActionEvent event) {
+
+    }
+
+    @FXML
+    void verwijderen_combobox(ActionEvent event) {
+
+    }
+
+    @FXML
+    void gebruikersnaam_tekst(ActionEvent event) {
+
+    }
+
+    @FXML
+    void wachtwoord_tekst(ActionEvent event) {
+
+    }
+
+    @FXML
+    void admin_tekst(ActionEvent event) {
 
     }
 }
