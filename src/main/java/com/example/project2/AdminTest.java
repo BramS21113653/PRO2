d596026d03654c7041e02ab370bd67a5eae72692
LucvanKoppenUdoc <luc@udoc.nl>
2022-06-17 18:49:29 +0200

    test

--- src/main/java/com/example/project2/AdminTest.java
+++ src/main/java/com/example/project2/AdminTest.java
@@ -3,19 +3,30 @@ package com.example.project2;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import static org.junit.Assert.*;
 
 public class AdminTest {
-    protected static Connection connection = .get;
+    protected static Connection connection = Database.getConnection();
 
 
     @Test
     public void insertGebruiker() throws SQLException {
     Client gebruiker = new Client(9389488, "test", "test", 0, 99, true);
     gebruiker.insertGebruiker();
-
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet result = statement.executeQuery("SELECT * FROM `gebruiker` ORDER BY `punten`");
+            System.out.println(result);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            if (connection != null) {
+            }
+        }
 
 
     }
