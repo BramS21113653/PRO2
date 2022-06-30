d1429033c32f6b54038a96aec08196e62d4f71b3
BramS21113653 <101334896+BramS21113653@users.noreply.github.com>
2022-06-30 13:06:04 +0200

    final version

--- src/main/java/com/example/project2/Database.java
+++ src/main/java/com/example/project2/Database.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class    Database {
+public class    Database implements IDatabase {
     public static Connection makeConnection() {
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/betabit", "root", "root");
